0aaf679b36a87f4e3a9a7a4bb1aa1b12b711169e
danielchild1 <danielchild1@DanielChildLaptop.localdomain>
2021-03-06 20:42:07 -0700

    added locked nodes

---
+++ Concurrent Linked Lists.cpp
@@ -0,0 +1,75 @@
+﻿// Concurrent Linked Lists.cpp : Defines the entry point for the application.
+//
+
+#include "Concurrent Linked Lists.h"
+#include <iostream>
+#include <memory>
+#include <cstdio>
+#include <thread>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <time.h>
+#include <mutex>
+
+
+using std::mutex;
+using std::thread;
+
+mutex myMutexx;
+
+unsigned int workerNumber = 0;
+
+void test() {
+	unsigned int localWorkerNumber;
+	linkedList<int> testLinkedList(true);
+
+	while (workerNumber <= 1000) {
+
+		myMutexx.lock();
+		workerNumber++;
+		localWorkerNumber = workerNumber;
+		myMutexx.unlock();
+
+		int action = rand() % 100;
+		int num = rand() % 99;
+
+		if (action <= 10) {
+			testLinkedList.insert(num);
+		}
+		else if (action <= 20) {
+			testLinkedList.remove(num);
+		}
+		else {
+			testLinkedList.find(num);
+		}
+
+		
+
+
+
+	}
+
+	
+
+
+
+
+}
+
+
+
+int main()
+{
+	srand(time(NULL));
+
+	thread* threads = new thread[4];
+
+	for (int i = 0; i < 4; i++) {
+		threads[i] = thread(test);
+	}
+	for (int i = 0; i < 4; i++) {
+		threads[i].join();
+	}
+
+	return 0;
+}
---
+++ Concurrent Linked Lists.h
@@ -0,0 +1,145 @@
+﻿// Concurrent Linked Lists.h : Include file for standard system include files,
+// or project specific include files.
+
+
+#include <memory>
+#include <cstdio>
+#include <atomic>
+#include <mutex>
+#include <shared_mutex>
+#include <thread>
+
+using std::atomic_compare_exchange_strong; 
+using std::atomic;
+using std::shared_ptr;
+using std::shared_mutex;
+using std::unique_lock;
+using std::shared_lock;
+using namespace std;
+
+
+template <typename T>
+class Node {
+public:
+	T value;
+	shared_ptr<Node<T>> next;
+	shared_ptr<Node<T>> prev;
+};
+
+template <typename T>
+class AtomicNode {
+public:
+	T value;
+	atomic<shared_ptr<AtomicNode<T>>> next;
+	atomic<shared_ptr<AtomicNode<T>>> prev;
+};
+
+
+template <typename T>
+class linkedList {
+private:
+	shared_ptr<Node<T>> front = std::make_shared<Node<T>>();
+	shared_ptr<Node<T>> back = std::make_shared<Node<T>>();
+	atomic<shared_ptr<AtomicNode<T>>> aFront;
+	atomic<shared_ptr<AtomicNode<T>>> aBack;
+	shared_mutex myMutex;
+	int numNodes = 0;
+	bool useLocks;
+
+
+
+	void appendLockFree(T newNode) {
+
+	}
+	void appendLocked(T newNode) {
+		shared_ptr<Node<T>> NewNode = std::make_shared<Node<T>>();
+		NewNode->value = newNode;
+
+		for (shared_ptr<Node<T>> temp = front; temp != back; temp = temp->next) {
+			if (temp->next == back) {
+				unique_lock lock(myMutex);
+				NewNode->next = back;
+				NewNode->prev = temp;
+				temp->next = NewNode;
+				back->prev = NewNode;
+			}
+			else if (newNode < temp->next->value) {
+				unique_lock lock(myMutex);
+				NewNode->next = temp->next;
+				NewNode->prev = temp;
+				temp->next->prev = NewNode;
+				temp->next = NewNode;
+				break;
+			}
+		}
+	}
+
+
+	void removeLockFree(T removalNode) {
+
+	}
+	void removeLocked(T removealNode) {
+		for (shared_ptr<Node<T>> temp = front; temp != back; temp = temp->next) {
+			if (temp->value == removealNode) {
+				unique_lock lock(myMutex);
+				temp->prev->next = temp->next;
+				temp->next->prev = temp->prev;
+				numNodes--;
+				break;
+			}
+		}
+	}
+
+	bool findLocked(T val) {
+		for (shared_ptr<Node<T>> temp = front; temp != back; temp = temp->next) {
+			if (temp->value == val) {
+				return true;
+			}
+			if (temp->value > val) {
+				return false;
+			}
+		}
+		return false;
+	}
+
+public:
+	linkedList(bool locks ): useLocks(locks) {
+		if (locks) {
+			front->next = back;
+			back->prev = front;
+		}
+		else {
+	
+		}
+	}
+	void insert(T newNode) {
+		numNodes++;
+		if (useLocks) {
+			appendLocked(newNode);
+		}
+		else {
+			appendLockFree(newNode);
+		}
+	}
+
+	bool find(T value) {
+		shared_lock lock(myMutex);
+		if (useLocks) {
+			return findLocked(value);
+		}
+		return false;
+	}
+
+	void remove(T removalNode) {
+		if (useLocks) {
+			removeLocked(removalNode);
+		}
+		else {
+			//removeLockedFree(removalNode);
+		}
+	}
+
+};
+
+
+// TODO: Reference additional headers your program requires here.
